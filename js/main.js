5e9a1b854d1d4c476e9c8afdd7a06e39ef8f2724
Arminas <arminasb7@gmail.com>
2016-04-23 13:54:52 +0100

    Initial Commit
    Get JSON from Twitch API and display it.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 var streams = ["freecodecamp", "storbeck", "ogamingsc2"];
 for (var i=0; i<streams.length; i++) {
-$.getJSON('https://api.twitch.tv/kraken/streams/' + streams[i] + '?callback=?', function(data) {
+$.getJSON('https://api.twitch.tv/kraken/channels/' + streams[i] + '?callback=?', function(data) {
+	if(data.status == null) {
+  	data.status = "offline";
+  	}
+  	$("div#data").append("<div class='streams'>" + 
+  		"<img src='" + 
+  		data.logo + 
+  		"' alt='Logo image'> " +
+  		data.display_name + " " + 
+  		data.status + 
+  		"</div>");
+
   console.log(data);
   });
 } //EOF streams
