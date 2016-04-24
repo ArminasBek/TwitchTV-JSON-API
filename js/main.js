e35ae3e29a8b2593db04325538904207db698bce
Arminas <arminasb7@gmail.com>
2016-04-24 14:34:04 +0100

    Initial Commit
    Basic functionality

--- js/main.js
+++ js/main.js
@@ -1,17 +1,49 @@
-var streams = ["freecodecamp", "storbeck", "ogamingsc2"];
-for (var i=0; i<streams.length; i++) {
-$.getJSON('https://api.twitch.tv/kraken/channels/' + streams[i] + '?callback=?', function(data) {
-	if(data.status == null) {
-  	data.status = "offline";
-  	}
-  	$("div#data").append("<div class='streams'>" + 
-  		"<img src='" + 
-  		data.logo + 
-  		"' alt='Logo image'> " +
-  		data.display_name + " " + 
-  		data.status + 
-  		"</div>");
+var streams = ["freecodecamp", "storbeck", "ogamingsc2", "comster404"];
+var streamVal;
+//Get info from Twitch.tv API
+	for (var i = 0; i < streams.length; i++) {
+		$.getJSON('https://api.twitch.tv/kraken/channels/' + streams[i] +'?callback=?', function(data){
+			//Check if user is online/offline or account is closed
+			if(data.status == null) {
+				streamVal = "Offline";
+			} else {
+				streamVal = data.status;
+			}
 
-  console.log(data);
-  });
-} //EOF streams
+			if(data.status == 422) {
+				$("div#data").append(data.message);
+			}else {
+			$("div#data").append("<div class='item'>" + data.display_name + ": " + " " + streamVal + "</div>")
+			console.log(data);
+			}
+		})
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+// for (var i=0; i<streams.length; i++) {
+// $.getJSON('https://api.twitch.tv/kraken/channels/' + streams[i] + '?callback=?', function(data) {
+//   	console.log(data);	
+//   });
+
+// })
+
+
+// } //EOF streams
+// }); //EOF document ready
+
+
