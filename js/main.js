5130b7fce22a15b73342216eaa43ac923fc263bb
Arminas <arminasbek@android-e12776e4c3b0ff84.home>
2016-04-26 08:42:00 +0100

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,22 +2,8 @@ var streams = ["freecodecamp", "storbeck", "ogamingsc2", "comster404"];
 var streamVal;
 //Get info from Twitch.tv API
 for (var i = 0; i < streams.length; i++) {
-	$.getJSON('https://api.twitch.tv/kraken/channels/' + streams[i] +'?callback=?', function(data){
-		//Check if user is online/offline
-		if(data.status == null) {
-			streamVal = "Offline";
-		} else {
-			streamVal = data.status;
-		}
-		//Check if user account available and display output
-		if(data.status == 422) {
-			streamVal = "Account is closed";
-			$("div#data").append(data.message);
-			}else {
-			$("div#data").append("<div class='item'>" + data.display_name + ": " + " " + streamVal + "</div>")
-			console.log(data);
-			}
-		})
+	$.getJSON('https://api.twitch.tv/kraken/streams/' + streams[i] +'?callback=?', function(data){
+		console.log(data);
 	} //EOF for statement
 
 
