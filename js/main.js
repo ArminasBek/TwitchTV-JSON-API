37668d362fa1f32c50a6bcb1d2af109085da08b3
Arminas <arminasb7@gmail.com>
2016-04-26 10:21:43 +0100

    Initial Commit
    Cycle through each array and add values

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 var streams = ["freecodecamp", "storbeck", "ogamingsc2", "comster404"];
 var streamVal;
+var html;
 //Get info from Twitch.tv API
-for (var i = 0; i < streams.length; i++) {
-	$.getJSON('https://api.twitch.tv/kraken/streams/' + streams[i] +'?callback=?', function(data){
+function getInfo(){
+	for (var i = 0; i < streams.length; i++) {
+		(function(i){
+		$.getJSON('https://api.twitch.tv/kraken/streams/' + streams[i] +'?callback=?', function(data){
+		if(data.stream == null){
+			streamVal = "Offline";
+		} else {
+			streamVal = data.stream.channel.status;
+		}
+
+		if(data.status == 422) {
+			streamVal = "Channel is unavailable";
+		}
+
+		html = '<div class="item">' + streams[i] + ' : ' + streamVal + '</div>'
+
+		$("div#data").append(html);
 		console.log(data);
-	} //EOF for statement
+	})//EOF getJSON
+	})(i);
+	}//EOF for statement
+}//EOF getInfo();
+
+
+getInfo();
+
+
+
 
 
